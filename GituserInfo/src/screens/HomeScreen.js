9af515901d03b1ed7937611b23224a8e64206f6d
Poojasevak <spooja.uidev@gmail.com>
2023-05-04 14:24:36 +0530

    Implemented debouncing for the search

--- GituserInfo/src/screens/HomeScreen.js
+++ GituserInfo/src/screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet, BackHandler} from 'react-native';
 import {Searchbar} from 'react-native-paper';
 import UserDetails from '../components/user-components/UserDetails';
 import {defineIcon} from '../assets/svg';
 import {Colors} from '../styles';
 import LinearGradient from 'react-native-linear-gradient';
+import ConfirmExitModal from '../components/ common-components/ConfirmExitModal';
 
 const styles = StyleSheet.create({
   homeContainer: {
@@ -15,7 +16,46 @@ const styles = StyleSheet.create({
 });
 const HomeScreen = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const onChangeSearch = query => setSearchQuery(query);
+  const [deboucedkey, setDeboucedkey] = useState('');
+
+  const [showCloseModal, setShowCloseModal] = useState(false);
+
+  const backActionHandler = () => {
+    setShowCloseModal(true);
+    return <ConfirmExitModal />;
+  };
+
+  useEffect(() => {
+    // Add event listener for hardware back button press on Android
+    BackHandler.addEventListener('hardwareBackPress', backActionHandler);
+    return () =>
+      // clear/remove event listener
+      BackHandler.removeEventListener('hardwareBackPress', backActionHandler);
+  }, []);
+
+  const debounce = (func, delay) => {
+    let timeoutId;
+    return (...args) => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      timeoutId = setTimeout(() => {
+        func.apply(null, args);
+      }, delay);
+    };
+  };
+
+  const onChangeSearch = query => {
+    setSearchQuery(query);
+  };
+
+  useEffect(() => {
+    const delayedSearch = debounce(() => {
+      console.log(`Searching for ${searchQuery}`);
+      setDeboucedkey(searchQuery);
+    }, 500);
+    delayedSearch();
+  }, [searchQuery]);
 
   useEffect(() => {}, [searchQuery]);
 
@@ -35,8 +75,18 @@ const HomeScreen = () => {
         icon={() => <View>{defineIcon('searchbar', 20, 20)}</View>}
         clearIcon={() => <View>{defineIcon('close', 20, 20)}</View>}
       />
-
-      <UserDetails username={searchQuery} />
+      {showCloseModal ? (
+        <ConfirmExitModal
+          onCancel={() => {
+            setShowCloseModal(false);
+          }}
+          onConfirm={() => {
+            setShowCloseModal(false);
+            BackHandler.exitApp();
+          }}
+        />
+      ) : null}
+      <UserDetails username={deboucedkey} />
     </LinearGradient>
   );
 };
